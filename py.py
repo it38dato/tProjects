1736890aee2c3153a3c75a6bfc30fd0552bd80a0
dato38it <Dato38it@yandex.ru>
2024-04-24 10:02:15 +0800

    add readme and correct python programm

--- py.py
+++ py.py
@@ -1,20 +1,12 @@
-# fr - read file
-# fo - output file
-# fw - write file
-# l - line
-# rt - replace tab
-tags=["<p>","</p>","<strong>","</strong>","</br>","&nbsp"]
+tags=["<p>","</p>","<strong>","</strong>","<br>","&nbsp;&nbsp;&nbsp;"]
 #print(tags[2])
-fr = open("text.txt", "r") 
-#fo=fr.read()
-#print(fo)
-fe = open("editor.txt", "w")
-#fe.write(fo)
-for l in fr:
-    #print(l, end="")   
-    rt=l.replace("    ", tags[5])    
-    fe.write(rt)
-    rn=l.join([l.strip() + tags[4]])
-    fe.write(rn)
-fe.close()
-fr.close()
+with open("text.txt") as fe:
+    fr = fe.read()
+    #print(fr)
+rtn = fr.replace("    ", tags[5]).replace("\n", tags[4])
+#print(rtn)
+#print(tags[0], rtn, tags[1])
+rtn1 = tags[0]+rtn+tags[1]
+#print(rtn1)
+with open("output.txt", "w") as fe:
+    fe.write(rtn1+"\n")
